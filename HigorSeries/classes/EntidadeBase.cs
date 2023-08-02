7e13c881030cac91a1b7c7840f1bd76c836e205b
Higor Albuquerque <89108002+higor957@users.noreply.github.com>
2023-08-01 23:57:28 -0300

    Add files via upload
    Projeto em DOTNET de cadastro de séries para aprendizado

---
+++ HigorSeries/classes/EntidadeBase.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Higor.Series
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { protected set; get; }
+
+    }
+
+}
